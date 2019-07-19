5a9a895524f47750cb4327734717851b92c881d6
Leandro Santos <santosdleandro@hotmail.com>
2019-07-19 11:47:30 -0300

    Update iniciar_sessao.py

--- iniciar_sessao.py
+++ iniciar_sessao.py
@@ -17,11 +17,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep, strftime
 from random import randint
+import schedule
+import time
 
 
 # credenciais de login
 insta_username = 'Rodolfodsantos2019'
 insta_password = '1e12e1e1'
+
+#Supervisor de cotas
+"""Assuma o controle total das ações com as estratégias mais sofisticadas"""
+
+session.set_quota_supervisor(enabled=True, sleep_after=["likes", "comments_d", "follows", "unfollows", "server_calls_h"], sleepyhead=True, stochastic_flow=True, notify_me=True,
+                              peak_likes=(57, 585),
+                               peak_comments=(21, 182),
+                                peak_follows=(48, None),
+                                 peak_unfollows=(35, 402),
+                                  peak_server_calls=(None, 4700))
+
 
 # obtenha uma sessão InstaPy!
 # set headless_browser = True para executar o InstaPy em segundo plano
@@ -29,11 +42,10 @@ session = InstaPy(username=insta_username,
                   password=insta_password,
                   headless_browser=False)
                   
-with smart_run(session):
+with smart_run(session, threaded=True):
     """ Activity flow """
     # general settings
     session.set_relationship_bounds(enabled=True,
-                                    potency_ratio = -1.3,
                                     delimit_by_numbers=True,
                                     max_followers = 3000 ,
                                     max_following = 2000 ,
@@ -74,15 +86,38 @@ with smart_run(session):
     session.follow_user_followers(['marcelo_de_brito'], amount=3500,
                                   randomize=True, interact=False)
 
+    #Não pare de seguir usuários ativos
+    """Impede os seguidores que deixaram de seguir um de seus últimos 50 posts
+    """
+    session.set_dont_unfollow_active_users ( enabled = True , posts = 50 )
+    
+    #Simulaçao
+    """Durante a recuperação indireta de dados, a simulação fornece um fluxo de atividade genuíno acionado por um algoritmo inteligente.
+    """
+    session.set_simulation ( enabled = True , percentage = 66 )    
+
+    #Ignorando usuário para conta particular, sem foto de perfil, conta comercial
+    """Isso é usado para pular usuários com certas condições
+    """
+    session.set_skip_users ( skip_private = False ,
+                             skip_no_profile_pic = Verdadeiro ,
+                             no_profile_pic_percentage = 100 )
+    
+    
     # UNFOLLOW atividade
     "" " Deixar de seguir não seguidores depois de um dia ...
     "" "
     session.unfollow_users(amount=random.randint(75, 100),
                            InstapyFollowed=(True, "nonfollowers"),
                            style="FIFO",
-                           unfollow_after=48 * 60 * 60, sleep_delay=600)
+                           unfollow_after=48 * 60 * 60, sleep_=600)
     #Atrasos
-    #session.set_action_delays(enabled=True, like=5.2, randomize=True, random_range=(70, 140))
+    """Depois de fazer cada ação, comentar, seguir ou deixar de seguir, há um atraso de sono para fornecer um fluxo de atividade suave .
+    """
+    session.set_action_delays(enabled=True,
+                              randomize = True , 
+                              random_range = ( 70 , 140 ))
+                                                            
 
     """ Segundo passo do Massive Follow ...
     """
